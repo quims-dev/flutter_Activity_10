dc00515d76b2878be616a8d922cab73055362f4f
quims-dev <quims.dev@gmail.com>
2025-11-24 21:18:48 +0800

    Added favorite filter

--- lib/crud_service.dart
+++ lib/crud_service.dart
@@ -1,38 +1,51 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class CrudService{
-  final CollectionReference items = 
-  FirebaseFirestore.instance.collection('items');
-
-  Future <void> addItem(String name, int quantity) {
+class CrudService {
+  final CollectionReference items =
+      FirebaseFirestore.instance.collection('items');
 
 
-
+  Future<void> addItem(String name, int quantity) {
     return items.add({
       'name': name,
       'quantity': quantity,
+      'favorite': false,
       'createdAt': Timestamp.now(),
     });
   }
 
 
-
-Stream<QuerySnapshot> getItems() {
-  return items.orderBy('createdAt', descending: true).snapshots();
-}
-
-
-Future <void> updateItem(String id, String name, int quantity){
-  return items.doc(id).update({
-    'name': name,
-    'quantity': quantity,
-  });
-}
+  Stream<QuerySnapshot> getItems() {
+    return items.orderBy('createdAt', descending: true).snapshots();
+  }
 
 
+  Stream<QuerySnapshot> getItemsFiltered(bool favoritesOnly) {
+    if (favoritesOnly) {
+      return items
+          .where('favorite', isEqualTo: true)
+          .orderBy('createdAt', descending: true)
+          .snapshots();
+    }
+    return getItems();
+  }
 
- Future <void> deleteItem(String id) {
-  return items.doc(id).delete();
- }
 
+  Future<void> updateItem(String id, String name, int quantity) {
+    return items.doc(id).update({
+      'name': name,
+      'quantity': quantity,
+    });
+  }
+
+
+  Future<void> toggleFavorite(String id, bool currentValue) {
+    return items.doc(id).update({
+      'favorite': !currentValue,
+    });
+  }
+
+  Future<void> deleteItem(String id) {
+    return items.doc(id).delete();
+  }
 }
